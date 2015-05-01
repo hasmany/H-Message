ec9c7570a585952b4be8a23f2f237ee295d56638
Hubert Nguyen <hubertanguyen@gmail.com>
2015-05-01 10:31:55 -0500

    Create server, and helper functions to server static files

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var cache = {};
 function send404(response) {
   response.writeHead(404,{'Content-Type': 'text/plain'});
   response.write('Error 404: resource not found.');
-  resonse.end();
+  response.end();
 }
 
 // Handling serving of file data
@@ -36,3 +36,63 @@ function sendFile(response, filePath, fileContents) {
   );
   response.end(fileContents);
 }
+
+// Common for Node applicaton to cache frequently used data in memory
+// Our chat application will cache static files to memory, only reading them from the disk first time, page is loaded
+
+// Handles Caching, and serving files
+function serveStatic(response, cache, absPath) {
+  // If file is found in the cache, serve it
+  if (cache[absPath]) {
+    sendFile(response,absPath, cache[absPath]);
+  // If file is nout in cache
+  } else {
+  // Check if file exists via callback function
+    fs.exists(absPath, function(exists) {
+      if (exists) {
+  // If file exists, read file from disk
+        fs.readFile(absPath, function(err,data) {
+  // Error Handling
+          if (err) {
+            send404(response)
+          } else {
+  // Set read file to cache, and serv file
+            cache[absPath] = data;
+            sendFile(response,absPath,data);
+          }
+        });
+  // If file does not exist, display error message
+      } else {
+        send404(response);
+      }
+    });
+  }
+}
+
+// Instantiate HTTP server
+
+// Create HTTP server, using anonymous function to define pre-requst behavior
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  // Determine file served by default aka /
+  if (request.url == '/') {
+    filePath = "public/index.html";
+  } else {
+  // Translate URL path to relative file path
+  // (public is where our files are stored)
+    filePath = "public" + request.url;
+  }
+  // Abs path appends './', the root of our application
+  var absPath = "./" + filePath;
+  // Serve files based on absPath
+  serveStatic(response,cache,absPath);
+});
+
+// Starts the HTTP Server
+server.listen(3000,function(){
+  console.log("Server listening on port 3000.");
+});
+
+
+
